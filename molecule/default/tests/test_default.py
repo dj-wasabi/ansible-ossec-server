ba7f23b6ce1fd53b49417b7f0a5a86f92f0dbdc2
Werner Dijkerman <werner@dj-wasabi.nl>
2018-01-02 19:33:05 +0100

    Make socket test os independent

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -38,8 +38,11 @@ def test_ossec_verify_agent_conf(host):
     assert cmd.rc == 0
 
 
-def test_sockets_open(Socket):
-    assert Socket("tcp://:::1515").is_listening
+def test_sockets_open(Socket, SystemInfo):
+    if SystemInfo.distribution == 'debian':
+        assert Socket("tcp://0.0.0.0:1515").is_listening
+    elif SystemInfo.distribution == 'centos'::
+        assert Socket("tcp://:::1515").is_listening
 
 
 def test_ossec_configuration(File):
